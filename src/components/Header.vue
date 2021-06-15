cd1742ee521760302e3e581f0a20569c75ab022c
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-06-15 14:43:09 -0300

    add Header
    Criaçao do header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <p>PrimeTec</p>
+    <div id="nav">
+      <div class="d-flex mt-2">
+        <router-link to="/">Início</router-link>
+        <p>|</p>
+
+        <router-link to="/">Acompanhamento</router-link>
+      </div>
+      <div class="d-flex">
+        <button>Ordem de Serviço</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +20,42 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#nav {
+  align-items: flex-start;
+  padding: 20px;
+  background-color: #008037;
+  display: flex;
+  justify-content: space-between;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+}
+
+p {
+  margin-left: 5px;
+  margin-right: 5px;
+  font-weight: bold;
+  color: white;
+}
+
+button {
+  width: 230px;
+  height: 3rem;
+
+  margin-right: 2rem;
+
+  border: none;
+  border-radius: 8px;
+
+  font-weight: bold;
+
+  background-color: #5fcb5d;
+  color: white;
+}
+button:hover {
+  background-color: #85dd84;
+}
+</style>
